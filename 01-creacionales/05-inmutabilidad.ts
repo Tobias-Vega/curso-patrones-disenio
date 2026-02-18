2c37696b22581167387bec168e2f8c915c5818e8
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-18 17:49:21 -0300

    Inumtabilidad con copia

--- 01-creacionales/05-inmutabilidad.ts
+++ 01-creacionales/05-inmutabilidad.ts
@@ -6,3 +6,60 @@
  *  * Es útil para mantener un historial de estados en aplicaciones interactivas.
  *
  */
+
+class CodeEditorState {
+  readonly content: string;
+  readonly cursorPosition: number;
+  readonly unSaveChanged: boolean;
+
+  constructor(content: string, cursorPosition: number, unSavechanged: boolean) {
+    this.content = content;
+    this.cursorPosition = cursorPosition;
+    this.unSaveChanged = unSavechanged;
+
+  }
+
+  copyWith({
+    content,
+    cursorPosition,
+    unSaveChanged
+  }: Partial<CodeEditorState>): CodeEditorState {
+
+    return new CodeEditorState(
+      content ?? this.content,
+      cursorPosition ?? this.cursorPosition,
+      unSaveChanged ?? this.unSaveChanged,
+    )
+  }
+
+  displayState() {
+    console.log('\nEstado del editor');
+    console.log(`
+        Contenido: ${this.content}
+        Cursor Pos: ${this.cursorPosition}
+        Unsaved changes: ${this.unSaveChanged}
+      `)
+  }
+}
+
+
+class CodeEditorHistory {
+
+  private history: CodeEditorState[] = [];
+  private currentIndex: number = -1;
+
+  save(state: CodeEditorState): void {
+    this.history.push(state);
+    this.currentIndex++;
+  }
+
+  redo(): CodeEditorState | null {
+    if (this.currentIndex < this.history.length -1) {
+      this.currentIndex++;
+      return this.history[this.currentIndex];
+    }
+
+    return null;
+  }
+
+}
