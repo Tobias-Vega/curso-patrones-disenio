e1ec748fc6eee6e50645d210f0581295140bcdb3
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-18 17:35:30 -0300

    Patrón Prototype - Solución

--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -9,3 +9,49 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+interface Prototype {
+  clone(): Prototype;
+}
+
+class Pokemon implements Prototype {
+  public name: string;
+  public type: string;
+  public level: number;
+  public attacks: string[];
+
+  constructor(name: string, type: string, level: number, attacks: string[]) {
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.attacks = attacks;
+  }
+
+  // Método para clonar el Pokémon
+  public clone(): Pokemon {
+
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks]);
+  }
+
+  displayInfo(): void {
+    console.log(
+      `Nombre: ${this.name}\nTipo: ${this.type}\nNivel: ${this.level
+      }\nAtaques: ${this.attacks.join(', ')}`
+    );
+  }
+}
+
+// Tarea:
+// 1. Crear un Pokémon base.
+// 2. Clonar el Pokémon base y modificar algunos atributos en los clones.
+// 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles.
+
+// Ejemplo:
+const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
+const clone1 = basePokemon.clone();
+clone1.name = "Charmeleon";
+clone1.level = 16;
+clone1.attacks.push("Lanzallamas");
+
+basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+clone1.displayInfo();
