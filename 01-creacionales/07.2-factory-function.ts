fd0e1b177bd33a4738cc0473cb357bc81c72429b
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-20 16:39:58 -0300

    Factory Function - Solución

--- 01-creacionales/07.2-factory-function.ts
+++ 01-creacionales/07.2-factory-function.ts
@@ -13,3 +13,45 @@
 //* [INFO:2025-10-21:07] Aplicación iniciada correctamente.
 //* [WARNING:2025-10-21:07] El uso de memoria está alto.
 //* [ERROR:2025-10-21:07] Error de conexión a la base de datos.
+
+function formatDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses empiezan desde 0
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+// Función fábrica que crea un manejador de logs
+type LogLevel = 'info' | 'warn' | 'error';
+
+function createLogger(level: LogLevel) {
+  // Retorna una función que recibe el "message" como argumento
+  return function(message: string) {
+    const timestamp = formatDate(new Date());
+
+    const messages = {
+      info: `${timestamp}  ${message}`,
+      warn: `${timestamp}  ${message}`,
+      error: `${timestamp}  ${message}`,
+    }
+
+    return console.log(messages[level])
+  }
+}
+
+// Ejemplo de uso
+function main() {
+  const infoLogger = createLogger('info');
+  const warnLogger = createLogger('warn');
+  const errorLogger = createLogger('error');
+
+  infoLogger('Aplicación iniciada correctamente.');
+  warnLogger('El uso de memoria está alto.');
+  errorLogger('Error de conexión a la base de datos.');
+}
+
+main();
