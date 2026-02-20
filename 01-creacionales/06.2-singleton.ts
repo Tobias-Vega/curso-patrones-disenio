e34c9a3b09d1d814427195b4ecfec49ed85f05bc
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-20 15:40:14 -0300

    Patrón Singleton - Solución

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -7,3 +7,62 @@
  * * de una clase, como por ejemplo, en un objeto de base de datos o en un
  * * objeto de configuración.
  */
+
+class DatabaseConnection {
+  private static instance: DatabaseConnection;
+  private connected: boolean = false;
+
+  // Constructor privado para evitar instancias directas
+  private constructor() { }
+
+  // Método estático para obtener la instancia única
+  public static getInstance(): DatabaseConnection {
+    if (!DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+      return DatabaseConnection.instance;
+    }
+    return DatabaseConnection.instance;
+
+  }
+
+  // Método para conectar a la base de datos
+  public connect(): void {
+    if (this.connected) {
+      console.log('Ya estabamos conectado a la Base de Datos');
+      return;
+    }
+
+    this.connected = true;
+    console.log('Nueva conexión a la Base de Datos');
+  }
+
+  // Método para desconectar de la base de datos
+  public disconnect(): void {
+    // Completar: desconectar y mostrar mensaje de desconexión
+    if (this.connected) {
+      console.log('Desconetamos la conexión de la Base de Datos');
+      this.connected = false;
+      return;
+    }
+
+    console.log('No hay una conexión activa');
+  }
+}
+
+// Pruebas
+function main() {
+  const db1 = DatabaseConnection.getInstance();
+  db1.connect(); // Debería conectar a la base de datos
+
+  const db2 = DatabaseConnection.getInstance();
+  db2.connect(); // Debería mostrar que ya existe una conexión activa
+
+  console.log('Son iguales:', db1 === db2); // Debería mostrar true
+
+  db1.disconnect(); // Debería cerrar la conexión
+  db2.disconnect();
+
+  db2.connect(); // Ahora debería conectar de nuevo, ya que se cerró la anterior
+}
+
+main();
